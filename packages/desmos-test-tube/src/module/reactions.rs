d37567ab0fdb65b9cee7339c3facbf1ca3fcdcd8
Manuel Turetta <manuel.turetta94@gmail.com>
2023-08-17 16:24:18 -0500

    test: add reactions module tests

--- packages/desmos-test-tube/src/module/reactions.rs
+++ packages/desmos-test-tube/src/module/reactions.rs
@@ -53,3 +53,179 @@ where
         pub query_params ["/desmos.reactions.v1.Query/ReactionsParams"]:  QueryReactionsParamsRequest => QueryReactionsParamsResponse
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::module::test_utils::*;
+    use crate::module::Reactions;
+    use crate::runner::app::DesmosTestApp;
+    use cosmwasm_std::{coin, Addr};
+    use desmos_bindings::reactions::msg::ReactionsMsg;
+    use desmos_bindings::reactions::types::{
+        FreeTextValue, QueryReactionRequest, QueryReactionsRequest, QueryRegisteredReactionRequest,
+        QueryRegisteredReactionsRequest, ReactionValue,
+    };
+    use test_tube::{Account, Module};
+
+    #[test]
+    fn test_reaction_add_remove() {
+        let app = DesmosTestApp::new();
+        let account = app.init_account(&[coin(100_000_000_000, "udsm")]).unwrap();
+        let reactions = Reactions::new(&app);
+
+        // Prepare the environment
+        create_test_profile(&app, &account);
+        let subspace = create_test_subspace(&app, &account);
+        let post = create_test_post(&app, &account, subspace.id);
+
+        // Add a reaction to the created post.
+        let created_reaction_id = reactions
+            .add_reaction(
+                ReactionsMsg::add_reaction(
+                    subspace.id,
+                    post.id,
+                    ReactionValue::FreeText(FreeTextValue {
+                        text: "Like".to_string(),
+                    }),
+                    Addr::unchecked(account.address()),
+                ),
+                &account,
+            )
+            .unwrap()
+            .data
+            .reaction_id;
+
+        // Test reaction remove
+        reactions
+            .remove_reaction(
+                ReactionsMsg::remove_reaction(
+                    subspace.id,
+                    post.id,
+                    created_reaction_id,
+                    Addr::unchecked(account.address()),
+                ),
+                &account,
+            )
+            .unwrap();
+    }
+
+    #[test]
+    fn test_registered_reactions_add_edit_remove() {
+        let app = DesmosTestApp::new();
+        let account = app.init_account(&[coin(100_000_000_000, "udsm")]).unwrap();
+        let reactions = Reactions::new(&app);
+
+        // Prepare the environment
+        let subspace = create_test_subspace(&app, &account);
+
+        // Add a registered reaction
+        let registered_reaction_id = reactions
+            .add_registered_reaction(
+                ReactionsMsg::add_registered_reaction(
+                    subspace.id,
+                    "like",
+                    "+1",
+                    Addr::unchecked(account.address()),
+                ),
+                &account,
+            )
+            .unwrap()
+            .data
+            .registered_reaction_id;
+
+        // Try to edit the previously created reaction
+        reactions
+            .edit_registered_reaction(
+                ReactionsMsg::edit_registered_reaction(
+                    subspace.id,
+                    registered_reaction_id,
+                    "dislike",
+                    "-1",
+                    Addr::unchecked(account.address()),
+                ),
+                &account,
+            )
+            .unwrap();
+
+        // Remove the registered reaction
+        reactions
+            .remove_registered_reaction(
+                ReactionsMsg::remove_registered_reaction(
+                    subspace.id,
+                    registered_reaction_id,
+                    Addr::unchecked(account.address()),
+                ),
+                &account,
+            )
+            .unwrap();
+    }
+
+    #[test]
+    fn test_query_reaction() {
+        let app = DesmosTestApp::new();
+        let account = app.init_account(&[coin(100_000_000_000, "udsm")]).unwrap();
+        let reactions = Reactions::new(&app);
+
+        // Prepare the environment
+        create_test_profile(&app, &account);
+        let subspace = create_test_subspace(&app, &account);
+        let post = create_test_post(&app, &account, subspace.id);
+        let reaction = create_test_reaction(&app, &account, subspace.id, post.id);
+
+        // Test reaction query
+        let queried_reaction = reactions
+            .query_reaction(&QueryReactionRequest {
+                subspace_id: subspace.id,
+                post_id: post.id,
+                reaction_id: reaction.id,
+            })
+            .unwrap()
+            .reaction
+            .unwrap();
+        assert_eq!(reaction, queried_reaction);
+
+        // Test reactions query
+        let queried_reactions = reactions
+            .query_reactions(&QueryReactionsRequest {
+                subspace_id: subspace.id,
+                post_id: post.id,
+                user: account.address(),
+                pagination: None,
+            })
+            .unwrap()
+            .reactions;
+        assert_eq!(vec![reaction], queried_reactions);
+    }
+
+    #[test]
+    fn test_query_registered_reaction() {
+        let app = DesmosTestApp::new();
+        let account = app.init_account(&[coin(100_000_000_000, "udsm")]).unwrap();
+        let reactions = Reactions::new(&app);
+
+        // Prepare the environment
+        let subspace = create_test_subspace(&app, &account);
+        let registered_reaction = create_test_registered_reaction(&app, &account, subspace.id);
+
+        // Test registered reaction query
+        let queried_registered_reaction = reactions
+            .query_registered_reaction(&QueryRegisteredReactionRequest {
+                subspace_id: subspace.id,
+                reaction_id: registered_reaction.id,
+            })
+            .unwrap()
+            .registered_reaction
+            .unwrap();
+        assert_eq!(registered_reaction, queried_registered_reaction);
+
+        // Test registered reactions query
+        let queried_registered_reactions = reactions
+            .query_registered_reactions(&QueryRegisteredReactionsRequest {
+                subspace_id: subspace.id,
+                pagination: None,
+            })
+            .unwrap()
+            .registered_reactions;
+        assert_eq!(vec![registered_reaction], queried_registered_reactions);
+    }
+}
